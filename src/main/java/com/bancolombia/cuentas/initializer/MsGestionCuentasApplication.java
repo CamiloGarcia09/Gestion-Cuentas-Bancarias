54af44c6893f6963fb185a2192845938c2428bdf
Camilo Garcia <juan092005ca@gmail.com>
2025-11-29 15:31:41 -0500

    Initializer. Ajustes en la capa initializer para la correcta ejecuion del proyecto

--- src/main/java/com/bancolombia/cuentas/initializer/MsGestionCuentasApplication.java
+++ src/main/java/com/bancolombia/cuentas/initializer/MsGestionCuentasApplication.java
@@ -2,8 +2,14 @@ package com.bancolombia.cuentas.initializer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bancolombia.cuentas"})
+@EntityScan(basePackages = {"com.bancolombia.cuentas.application.secondaryports.entity"})
+@EnableJpaRepositories(basePackages = {"com.bancolombia.cuentas"})
 public class MsGestionCuentasApplication {
 
 	public static void main(String[] args) {
